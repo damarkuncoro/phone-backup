58405f3ac1666fe11f3ecd563981a6ccd6838307
Damar Kuncoro <damarkuncoro@gmail.com>
2026-09-03 10:50:44 +0700

    feat(application): integrate image-engine for EXIF parsing and media analysis

--- libs/core/application/src/analysis/media.rs
+++ libs/core/application/src/analysis/media.rs
@@ -1,5 +1,5 @@
-use chrono::{DateTime, Utc};
 use domain::MediaInfo;
+use image_engine::ExifReader;
 use std::io::Cursor;
 
 pub struct MediaAnalyzer;
@@ -15,54 +15,28 @@ impl MediaAnalyzer {
     }
 
     fn extract_image_info(data: &[u8]) -> Option<MediaInfo> {
-        let mut reader = Cursor::new(data);
-        if let Ok(exif_data) = exif::Reader::new().read_from_container(&mut reader) {
-            let mut info = MediaInfo::default();
+        let meta = ExifReader::read_from_bytes(data).ok()?;
+        let (lat, lon) = match meta.gps {
+            Some(ref gps) => (Some(gps.latitude), Some(gps.longitude)),
+            None => (None, None),
+        };
 
-            if let Some(field) = exif_data.get_field(exif::Tag::Make, exif::In::PRIMARY) {
-                info.camera_make = Some(field.display_value().to_string());
-            }
-            if let Some(field) = exif_data.get_field(exif::Tag::Model, exif::In::PRIMARY) {
-                info.camera_model = Some(field.display_value().to_string());
-            }
-            if let Some(field) = exif_data.get_field(exif::Tag::PixelXDimension, exif::In::PRIMARY)
-            {
-                info.width = field.value.get_uint(0);
-            }
-            if let Some(field) = exif_data.get_field(exif::Tag::PixelYDimension, exif::In::PRIMARY)
-            {
-                info.height = field.value.get_uint(0);
-            }
+        let (width, height) = if let Ok(img) = image::load_from_memory(data) {
+            (Some(img.width()), Some(img.height()))
+        } else {
+            (None, None)
+        };
 
-            if let Some(field) = exif_data.get_field(exif::Tag::DateTimeOriginal, exif::In::PRIMARY)
-            {
-                let date_str = field.display_value().to_string();
-                if let Ok(dt) =
-                    chrono::NaiveDateTime::parse_from_str(&date_str, "%Y:%m:%d %H:%M:%S")
-                {
-                    info.taken_at = Some(DateTime::<Utc>::from_naive_utc_and_offset(dt, Utc));
-                }
-            }
-
-            if let (Some(lat_field), Some(lat_ref), Some(lon_field), Some(lon_ref)) = (
-                exif_data.get_field(exif::Tag::GPSLatitude, exif::In::PRIMARY),
-                exif_data.get_field(exif::Tag::GPSLatitudeRef, exif::In::PRIMARY),
-                exif_data.get_field(exif::Tag::GPSLongitude, exif::In::PRIMARY),
-                exif_data.get_field(exif::Tag::GPSLongitudeRef, exif::In::PRIMARY),
-            ) {
-                info.latitude = Self::parse_gps_coordinate(
-                    lat_field,
-                    lat_ref.display_value().to_string().contains('S'),
-                );
-                info.longitude = Self::parse_gps_coordinate(
-                    lon_field,
-                    lon_ref.display_value().to_string().contains('W'),
-                );
-            }
-
-            return Some(info);
-        }
-        None
+        Some(MediaInfo {
+            camera_make: meta.camera.make,
+            camera_model: meta.camera.model,
+            width,
+            height,
+            taken_at: meta.date_taken,
+            latitude: lat,
+            longitude: lon,
+            duration_ms: None,
+        })
     }
 
     fn extract_video_info(data: &[u8]) -> Option<MediaInfo> {
@@ -87,22 +61,6 @@ impl MediaAnalyzer {
                 height,
                 ..Default::default()
             });
-        }
-        None
-    }
-
-    fn parse_gps_coordinate(field: &exif::Field, is_negative: bool) -> Option<f64> {
-        if let exif::Value::Rational(ref values) = field.value {
-            if values.len() >= 3 {
-                let d = values[0].to_f64();
-                let m = values[1].to_f64();
-                let s = values[2].to_f64();
-                let mut coord = d + (m / 60.0) + (s / 3600.0);
-                if is_negative {
-                    coord = -coord;
-                }
-                return Some(coord);
-            }
         }
         None
     }
